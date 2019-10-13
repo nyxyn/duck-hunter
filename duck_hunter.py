bb5ca995af472b129637731e13ef2ad95cfc03f2
Nyx <haxnyx@gmail.com>
2019-10-12 22:45:48 -0400

    Added duck hunter script

---
+++ duck_hunter.py
@@ -0,0 +1,34 @@
+import hexchat
+import threading
+import time
+
+TEXT_EVENT = 'Channel Message'
+DUCK_BOT = 'gonzobot'
+KEYWORD_1 = ' quack!'
+KEYWORD_2 = ' flap!'
+
+__module_name__ = 'duck_hunter'
+__module_version__ = '1.0'
+__module_description__ = 'Befriend that pesky duck'
+
+
+def say():
+    time.sleep(2)
+    hexchat.command('say .bef')
+
+
+def print_callback(words, eol, userdata):
+    nick = words[0]
+    message = words[1].replace(u'\u200b', '').lower()
+    
+    if DUCK_BOT in nick and (KEYWORD_1 in message or KEYWORD_2 in message):
+        thread = threading.Thread(target=say)
+        thread.start()
+
+    return hexchat.EAT_NONE
+
+
+hexchat.prnt('Setting hook for duck hunter...')
+hexchat.hook_print('Channel Message', print_callback)
+hexchat.prnt('Hook set')
+
