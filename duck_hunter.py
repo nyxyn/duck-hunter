871ba130bf3d5406d6a3b1c39665ce2a662a5fcc
Nyx <haxnyx@gmail.com>
2019-10-16 21:44:11 -0400

    Added saying key word in correct channel

--- duck_hunter.py
+++ duck_hunter.py
@@ -7,24 +7,28 @@ TEXT_EVENT = 'Channel Message'
 DUCK_BOT = 'gonzobot'
 KEYWORD_1 = ' quack!'
 KEYWORD_2 = ' flap!'
+CHANNEL_KEY = 'channel'
 
 __module_name__ = 'duck_hunter'
 __module_version__ = '1.0'
 __module_description__ = 'Befriend that pesky duck'
 
 
-def say():
+def say(channel):
     sleep_delay = random.randint(4, 15)
     time.sleep(sleep_delay)
-    hexchat.command('say .bef')
+
+    command = 'msg {} .bef'.format(channel)
+    hexchat.command(command)
 
 
 def print_callback(words, eol, userdata):
     nick = words[0]
     message = words[1].replace(u'\u200b', '').lower()
-    
+    channel = hexchat.get_info(CHANNEL_KEY)
+
     if DUCK_BOT in nick and (KEYWORD_1 in message or KEYWORD_2 in message):
-        thread = threading.Thread(target=say)
+        thread = threading.Thread(target=say, args=(channel,))
         thread.start()
 
     return hexchat.EAT_NONE
